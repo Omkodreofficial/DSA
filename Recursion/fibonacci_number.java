cd64700465e988a850749a9fd789d269613e7e96
OmKodreofficial <omkodre10423@gmail.com>
2026-06-14 11:56:00 +0530

    Implemented Fibonacci series using recursion

---
+++ Recursion/fibonacci_number.java
@@ -0,0 +1,14 @@
+public class fibonacci_number {
+    public static int fib(int n){
+        if(n<=1){
+            return n;
+        }
+        int last = fib(n-1);
+        int slast = fib(n-2);
+        return last+slast;
+    }
+    public static void main(String[] args) {
+        int ans = fib(4);
+        System.out.println(ans);
+    }
+}
