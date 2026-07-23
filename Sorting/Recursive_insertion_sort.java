a19008d4622b74d9f84fe2603103a453154224f8
OmKodreofficial <omkodre10423@gmail.com>
2026-07-23 12:47:35 +0530

    Implement recursive Insertion Sort algorithm

---
+++ Sorting/Recursive_insertion_sort.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Recursive_insertion_sort {
+
+    static void insertion_sort(int arr[], int i, int n) {
+
+        if (i >= n) {
+            return;
+        }
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+
+        insertion_sort(arr, i + 1, n);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the number of array elements : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        insertion_sort(arr, 0, n);
+
+        System.out.println("the sorted array is : ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
