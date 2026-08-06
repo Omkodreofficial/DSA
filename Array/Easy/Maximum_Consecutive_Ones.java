07f1b6fe3b1a942f97a2e9ab01e09f5808f8b220
OmKodreofficial <omkodre10423@gmail.com>
2026-08-06 20:16:35 +0530

    Add solution for maximum consecutive ones in array

---
+++ Array/Easy/Maximum_Consecutive_Ones.java
@@ -0,0 +1,21 @@
+public class Maximum_Consecutive_Ones {
+    static int maximum(int[] arr) {
+        int max = 0;
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                count++;
+                max = Math.max(max, count);
+            } else {
+                count = 0;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 1, 0, 1, 1, 1, 0, 1, 1 };
+        int ans = maximum(arr);
+        System.out.println(ans);
+    }
+}
